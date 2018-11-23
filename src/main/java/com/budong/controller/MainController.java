14c6b9bd627938c9e09e37687f5ff3a8bfb557eb
HyunWooKang <shortleader2@gmail.com>
2018-11-23 15:57:25 +0900

    news crawling add URL img add

--- src/main/java/com/budong/controller/MainController.java
+++ src/main/java/com/budong/controller/MainController.java
@@ -1,7 +1,15 @@
 package com.budong.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.budong.model.dto.NewsDTO;
+import com.budong.service.NewsServiceImpl;
 
 /**
  * 일반 사용자
@@ -9,8 +17,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class MainController {
+	
+	@Autowired
+	public NewsServiceImpl newsService;
+	
     @RequestMapping("/")
     public String goToIndexPage() {
         return "index";
     }
+    @RequestMapping("/title.news")
+    public ModelAndView goToTestPage(@RequestParam String param) {
+    	ModelAndView mav= new ModelAndView("news");
+    	ArrayList<NewsDTO> list = new ArrayList<>();
+    	String url = "https://land.naver.com/news/headline.nhn?bss_ymd="+param;
+    	list = newsService.getTitle(url);
+    	mav.addObject("list", list);
+    	return mav;
+    }
+    @RequestMapping("/content.news")
+    public ModelAndView getNewsbody(@RequestParam String param){
+    	ModelAndView mav= new ModelAndView("newsContent");
+    	String change = param.replaceAll("!", "&");
+    	mav.addObject("newsbody", newsService.getContent(change));
+    	return mav;
+    }
+    
+    
 }
